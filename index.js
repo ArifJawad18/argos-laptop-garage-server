3c4b544ea1834965fbcb691e8c11a36598b72a27
Arifjawad18 <arif.jawad18@gmail.com>
2022-12-05 01:16:08 +0600

    data add

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require( 'express');
+const cors = require('cors');
+
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+
+
+const app = express();
+
+//middleware
+app.use(cors())
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jpdjg4i.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+app.get('/', async(req, res) =>{
+    res.send('argos laptop garage port server is running');
+})
+
+app.listen(port, () => console.log(`argos laptop garage port server is running ${port}`))
