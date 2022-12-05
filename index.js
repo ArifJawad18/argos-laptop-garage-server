0c418c5561bde60302bc06e22cfc23d2a88dde80
Arifjawad18 <arif.jawad18@gmail.com>
2022-12-05 20:10:56 +0600

    order card

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require( 'express');
 const cors = require('cors');
-
-require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
@@ -15,7 +14,10 @@ app.use(cors())
 app.use(express.json());
 
 
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jpdjg4i.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
